a095731865dd47cf53c6cb958d3881bc88e2c4aa
rimbelkasmi <rimbelkasmi@gmail.com>
2026-03-18 12:26:51 +0000

    now

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,12 @@
-constassert= require('assert');
-const{ add} = require('../index');
-describe('Fonction add', () =>{
-it('devrait retourner 5 pour add(2, 3)', () =>{
-assert.strictEqual(add(2, 3), 5);
-});
-it('devrait retourner 0 pour add(-2, 2)', () =>{
-assert.strictEqual(add(-2, 2), 0);
-});
+const assert = require('assert');
+const add = require('../index'); // ou le bon chemin vers ta fonction
+
+describe('Fonction add', function () {
+  it('devrait retourner 5 pour add(2, 3)', function () {
+    assert.strictEqual(add(2, 3), 5);
+  });
+
+  it('devrait retourner 0 pour add(-2, 2)', function () {
+    assert.strictEqual(add(-2, 2), 0);
+  });
 });
